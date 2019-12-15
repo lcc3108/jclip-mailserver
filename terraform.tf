47f0515938fb32ad160570c51263b271319f8abc
IM CHAECHEOL <dlacocjf32@gmail.com>
2019-12-15 21:08:31 +0900

    .

--- terraform.tf
+++ terraform.tf
@@ -300,9 +300,9 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  # source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
 
-  # source_arn = "arn:aws:execute-api:us-east-1:906259781909:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:us-east-1:906259781909:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 resource "aws_lambda_permission" "with_lb" {
